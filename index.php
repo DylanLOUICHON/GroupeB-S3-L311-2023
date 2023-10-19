e90509b60ca0a962e51066d23cbaec4175cd3275
Dylan LOUICHON <147518224+DylanLOUICHON@users.noreply.github.com>
2023-10-19 17:32:00 +0200

    Update index.php
    Update

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Time</title>
-      <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
-      <link rel="stylesheet" href="assets/css/style.css">
-      <script src="assets/js/script.js"></script>
-  </head>
-  <body>
-    <div id="wrapper">
+    <head>
+        <meta charset="utf-8">
+        <title>Time</title>
+        <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet"> 
+        <link rel="stylesheet" href="assets/css/style.css">
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <!-- Timer -->
+            <div class="inside" id="wrapper">
+                <div class="timer">
+                    <span id="hours">00</span>
+                    <span id="minutes">00</span>
+                    <span id="seconds">00</span>
+                </div>
 
-      <!-- Timer -->
-      <div class="inside" id="wrapper">
-        <div class="timer">
-          <span id="hours">00</span>
-          <span id="minutes">00</span>
-          <span id="seconds">00</span>
+                <!-- Bouton pause du timer -->
+                <div class="controls">
+                    <button class="button pause"></button>
+                </div>
+            </div>
         </div>
-        
-        <!-- Bouton de pause du timer-->
-        <div class="controls">
-          <button class="button pause"></button>
-        </div>       
-      </div>
-    </div>
-  </body>
+    </body>
 </html>
