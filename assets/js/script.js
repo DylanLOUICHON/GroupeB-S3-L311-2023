31dd75408dc51d434d6f61701eaef5d3b2a55760
Dylan LOUICHON <147518224+DylanLOUICHON@users.noreply.github.com>
2023-10-19 17:45:46 +0200

    Update script.js
    Ajout commentaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
     init();
 
     function init() {
+        // Action du click sur le bouton
         document.querySelector('.button').addEventListener('click', (event) => {
             if (is_run) {
+                // Stop le timer et change l'icone
                 is_run = false;
                 document.querySelector('.button').classList.remove('pause');
             } else {
+                // Remet le timer et change l'icone
                 is_run = true;
                 document.querySelector('.button').classList.add('pause');
             }
         });
     }
 
+        // Génère le timer avec l'heure actuelle
         setInterval(function() {
             if (is_run) {
                 let oDate = new Date();
@@ -28,10 +32,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }, 1000);   
 });
 
+    // Fonction d'ajustement du timer
     function adjustTimer(timer) {
         return timer < 10 ? '0' + timer : timer;
     }
 
+    // Fonction de changement de couleur de fond
     function randomHexColor(x, y, z) {
         return "rgb(" + Math.floor(x/100 * 256) + "," + Math.floor(y/100 * 256) + "," + Math.floor(z/100 * 256) + ")";
     }
