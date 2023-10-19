01c1c67938b296f40095a3fb1678932c98057357
Dylan LOUICHON <147518224+DylanLOUICHON@users.noreply.github.com>
2023-10-19 17:33:26 +0200

    Update script.js
    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     let is_run = true;
+
     init();
 
     function init() {
-       document.querySelector('.button').addEventListener('click', (event) => 
+        document.querySelector('.button').addEventListener('click', (event) => {
             if (is_run) {
                 is_run = false;
                 document.querySelector('.button').classList.remove('pause');
@@ -13,20 +14,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
             }
         });
     }
-                        
+
         setInterval(function() {
             if (is_run) {
                 let oDate = new Date();
                 document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
                 document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
                 document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
-    
+
                 document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
                 
             }
-        }, 1000);
-    });
-
+        }, 1000);   
+});
 
     function adjustTimer(timer) {
         return timer < 10 ? '0' + timer : timer;
