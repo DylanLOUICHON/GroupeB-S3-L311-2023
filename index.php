7c7591bcb4cbf3779af77689d11d5f8fd1a89782
Dylan LOUICHON <147518224+DylanLOUICHON@users.noreply.github.com>
2023-10-19 17:00:08 +0200

    Correction index.php
    Correction des erreurs

--- index.php
+++ index.php
@@ -2,23 +2,27 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Timetitle>
-      <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet">
-      <link rel="stylesheet" href="asset/css/style.css">
+    <title>Time</title>
+      <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
+      <link rel="stylesheet" href="assets/css/style.css">
       <script src="assets/js/script.js"></script>
   </head>
   <body>
     <div id="wrapper">
+
+      <!-- Timer -->
       <div class="inside" id="wrapper">
         <div class="timer">
           <span id="hours">00</span>
           <span id="minutes">00</span>
           <span id="seconds">00</span>
         </div>
+        
+        <!-- Bouton de pause du timer-->
         <div class="controls">
           <button class="button pause"></button>
-        </div>
+        </div>       
       </div>
     </div>
-    <body>
+  </body>
 </html>
